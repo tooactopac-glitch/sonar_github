4664af8ab9ce532e2429f717ff2b9ad1e1f31893
DELL GPS <tooactopac@gmail.com>
2025-12-01 23:25:38 +0100

    v100

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-function sum(a,b,c,d,e,f,g,h,i,o,p,m,n,z,x,y) {
+function sum(a,b) {
     return a + b;
 }
 function echo(param){
     return param;
 }
-
-function login(user, password) {
-    // code here
-}
-
 module.exports = {sum}
