0e5899c38ecefc49d6e421129927c8f8078fff48
tooactopac-glitch <tooactopac@gmail.com>
2025-12-01 22:55:41 +0100

    v10

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function sum(a,b) {
+function sum(a,b,c,d,e,f,g,h,i,o,p,m,n,z,x,y) {
     return a + b;
 }
 function echo(param){
     return param;
+    console.log(param)
 }
-
+eval()
 
 
 const a = {
